9be6c4ddb0d5e4a9ed11219c6bd78d9fa1ee5f08
Jo <88668777+jonars99@users.noreply.github.com>
2022-08-14 16:27:22 +0100

    display mouse coordinates on div click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [ coords, setCoords ] = useState([]);
+
+  const handleClick = (e) => {
+    setCoords([e.pageX, e.pageY]);
+  }
 
   return (
     <div className="App App-header">
       <h2>Where in the Square ?</h2>
-      <div className="square"></div>
+      <div className="square" onClick={handleClick}></div>
+      <small>mouseX: {coords[0]}</small>
+      <small>mouseY: {coords[1]}</small>
     </div>
   );
 }
