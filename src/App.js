e31f945b33af1aab92f6954c51b06bce6ef44edd
Jo <88668777+jonars99@users.noreply.github.com>
2022-08-14 17:20:29 +0100

    draw line on canvas click

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  const [ coords, setCoords ] = useState([]);
+  const [ coords, setCoords ] = useState({
+    start: [],
+    end: [],
+  });
+  const c = useRef();
+
+  const drawLine = () => {
+    var ctx = c.current.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(coords.start[0], coords.start[1]);
+    ctx.lineTo(coords.end[0], coords.end[1]);
+    ctx.stroke();
+  }
 
   const handleClick = (e) => {
-    setCoords([e.pageX, e.pageY]);
+    const rect = e.target.getBoundingClientRect()
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    if (coords.start.length == 0) {
+      setCoords({
+        start: [Math.floor(x), Math.floor(y)],
+        end: [Math.floor(x), Math.floor(y)],
+      })
+    }
+    else {
+      setCoords({
+        start: [coords.end[0], coords.end[1]],
+        end: [Math.floor(x), Math.floor(y)],
+      })
+    }
   }
+
+  useEffect(() => {
+    drawLine();
+  }, [coords]);
 
   return (
     <div className="App App-header">
       <h2>Where in the Square ?</h2>
-      <canvas className="square" onClick={handleClick}></canvas>
-      <small>mouseX: {coords[0]}</small>
-      <small>mouseY: {coords[1]}</small>
+      <canvas width="350" height="350" ref={c} className="square" onClick={handleClick}></canvas>
+      <small>line start: x:{coords.start[0]}  y:{coords.start[1]}</small>
+      <small>line end: x:{coords.end[0]} y:{coords.end[1]}</small>
     </div>
   );
 }
