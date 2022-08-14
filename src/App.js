4e5df7193f41af88e285036c3c6ec6eeac0170c4
Jo <88668777+jonars99@users.noreply.github.com>
2022-08-14 16:15:49 +0100

    create white square

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <h2>Where in the Square ?</h2>
+      <div className="square"></div>
     </div>
   );
 }
